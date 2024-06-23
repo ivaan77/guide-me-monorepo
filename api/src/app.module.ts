36fac38f7b91c652d69eb681e473164cba64de48
Ivan Bošnjaković <Ivan.Bosnjakovic@infobip.com>
2024-06-23 22:37:47 +0200

    removed unecessary staff

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CityController } from './city/city.controller';
+import { CityService } from './city/city.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [CityController],
+  providers: [CityService],
 })
 export class AppModule {}
--- packages/core/build/constants/constants.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const BRAND_COLOR = "#06b6d4";
