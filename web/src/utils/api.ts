74cb2d766363f04649d2d60703cd254f78d5ae18
Ivan Bošnjaković <Ivan.Bosnjakovic@infobip.com>
2024-07-28 17:40:58 +0200

    build fixes

--- web/src/utils/api.ts
+++ web/src/utils/api.ts
@@ -1,24 +1,24 @@
 import { UploadResponse } from '@/app/api/upload/route';
 import { AllCityResponse, CityByIdResponse, CreateTourSpotRequest, SaveCityRequest, TourSpotResponse } from '@guide-me-app/core';
-import axios, { AxiosPromise } from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 const restClient = axios.create({ baseURL: '/api' });
 
-export const getAllCities = async (): AxiosPromise<AllCityResponse> => {
+export const getAllCities = async ():Promise<AxiosResponse<AllCityResponse>> => {
     return await restClient.get('/city');
 };
 
-export const saveCity = async (request: SaveCityRequest): AxiosPromise<CityByIdResponse> => {
+export const saveCity = async (request: SaveCityRequest): Promise<AxiosResponse<CityByIdResponse>> => {
     return await restClient.post('/city', request);
 };
 
-export const uploadFile = async (file: File): AxiosPromise<UploadResponse> => {
+export const uploadFile = async (file: File): Promise<AxiosResponse<UploadResponse>> => {
     const formData = new FormData();
     formData.set('file', file);
 
     return await restClient.post('/upload', formData);
 };
 
-export const saveTourSpot = async (request: CreateTourSpotRequest): AxiosPromise<TourSpotResponse> => {
+export const saveTourSpot = async (request: CreateTourSpotRequest): Promise<AxiosResponse<TourSpotResponse>> => {
     return await restClient.post('/tour/spot', request);
 };
