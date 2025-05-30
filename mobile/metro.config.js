dbf0082efe1f4f80339df789d5a4de7e65e2574f
Ivan Bosnjakovic <ivan.bosnjakovic7@gmail.com>
2025-05-30 18:53:51 +0200

    mobile initial setup

---
+++ mobile/metro.config.js
@@ -0,0 +1,17 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+/**
+ * @type {import('expo/metro-config').MetroConfig}
+ */
+const { getDefaultConfig } = require('expo/metro-config')
+const { withTamagui } = require('@tamagui/metro-plugin')
+
+const config = getDefaultConfig(__dirname, {
+  isCSSEnabled: false,
+})
+
+config.resolver.sourceExts.push('mjs')
+
+module.exports = withTamagui(config, {
+  components: ['tamagui'],
+  config: './tamagui.config.ts',
+})
